52f3e0904557acd2558359e8760612c759cf05dd
Sukhmanpreet Singh <ssbakshi2050@gmail.com>
2025-08-25 09:44:42 +0530

    Add files via upload

---
+++ R/Assignment 1/Assignment1.R
@@ -0,0 +1,58 @@
+# Assignment 1
+
+# Q1.
+A = 23.4;
+B = 45;
+C = 678;
+print(A);
+print(B);
+print(C);
+
+
+# Q2.
+ls();
+
+
+# Q3.
+rm(C);
+ls();
+
+
+# Q4.
+cat("I am learning R");
+
+
+# Q5.
+firstname = "Sukhmanpreet";
+lastname = "Singh";
+paste(firstname,lastname,sep=" ");
+
+
+# Q6.
+booleanVal = TRUE;
+flag = as.numeric(booleanVal);
+cat(booleanVal);
+cat(flag);
+
+
+# Q7.
+print(A+B+C);
+print(A-B+C);
+print(A*B*C);
+print(A/B*C);
+
+
+# Q8.
+exp(3);
+print(2^3);
+log(2);
+log10(10);
+pi
+sqrt(8);
+
+
+# Q9.
+print(23 + (4.5*2.3) / 10);
+print(456 / 12 - log(90));
+print(exp(5) + 12 / (5 ^ 6));
+print(sqrt(45) + 12 / 3);
